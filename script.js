16c63f45ebd31444b16a822e47da51c7495a82bb
Joao Vitor <joaovsantos.dev@gmail.com>
2025-03-11 13:29:55 -0300

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const parentButton = document.querySelectorAll("[data-cod]");
-const radioButtons = document.querySelectorAll("[type='radio']");
+const radioButton = document.querySelector("[type='radio']");
+
+radioButton.addEventListener('click', informaRetirada);
 
 parentButton.forEach((div) => {
   const dados = {};
@@ -14,33 +16,33 @@ function ativaAdd(div) {
   });
 }
 
-function plusOrMinus(div){
+function plusOrMinus(div) {
   const dados = {};
-  const inputQtd = div.querySelector('.btn-number input');
-  const buttonPlus = div.querySelector('.btn-number .plus');
-  const buttonMinus = div.querySelector('.btn-number .minus')
+  const inputQtd = div.querySelector(".btn-number input");
+  const buttonPlus = div.querySelector(".btn-number .plus");
+  const buttonMinus = div.querySelector(".btn-number .minus");
 
-  buttonPlus.addEventListener('click', () => {
+  buttonPlus.addEventListener("click", () => {
     inputQtd.value++;
     recebeDados(div, dados);
-  })
+  });
 
-  buttonMinus.addEventListener('click', () => {
-    inputQtd.value--;
-    recebeDados(div, dados);
-
-    if (inputQtd.value <= 0){
-      inputQtd.value = 0;
-      div.classList.remove('ativo');
+  buttonMinus.addEventListener("click", () => {
+    if (inputQtd.value < 1) {
+      div.classList.remove("ativo");
+      inputQtd.value++;
     }
-  })
+
+    inputQtd.value--;
+    recebeDados(div, dados)
+    
+  });
 }
 
-
-function recebeDados(div, dados){
-  const inputQtd = div.querySelector('.btn-number input');
-  const nomeProduto = div.querySelector('h1').innerText;
-  const precoProduto = +div.querySelector('.preco').innerText.replace(',', '.');
+function recebeDados(div, dados) {
+  const inputQtd = div.querySelector(".btn-number input");
+  const nomeProduto = div.querySelector("h1").innerText;
+  const precoProduto = +div.querySelector(".preco").innerText.replace(",", ".");
   const areaProdutos = document.querySelector(".produtos-selecionados");
 
   const info = div.dataset.cod;
@@ -52,43 +54,31 @@ function recebeDados(div, dados){
 
   let total = (dados.preco * dados.qtd).toFixed(2);
 
-  if (!elementoExistente && div.classList.contains('ativo')){
-    const novoElemento = document.createElement('div');
+  if (!elementoExistente && div.classList.contains("ativo")) {
+    const novoElemento = document.createElement("div");
     areaProdutos.appendChild(novoElemento);
     novoElemento.innerText = `${dados.nome} - x${dados.qtd} pacote(s) - R$${total}`;
-    novoElemento.setAttribute('data-id', `${info}`);
+    novoElemento.setAttribute("data-id", `${info}`);
   } else {
-    removeProduto(div, dados);
+    removeProduto(div, dados);a
     elementoExistente.innerText = `${dados.nome} - x${dados.qtd} pacote(s) - R$${total}`;
   }
 }
 
-function removeProduto(div, dados){
+function removeProduto(div, dados) {
   const info = div.dataset.cod;
   const elementoExistente = document.querySelector(`div[data-id="${info}"]`);
-  if(elementoExistente){
-    if (dados.qtd <= 0){
-      elementoExistente.remove()
+  if (elementoExistente) {
+    if (dados.qtd <= 0) {
+      elementoExistente.remove();
     }
   }
 }
 
-function ativaDadosEntrega(){
-  const parentDados = document.querySelector('.entrega-open');
-
-  radioButtons.forEach((button) => {
-    buttonValue = button.value;
+function informaRetirada(event) {
+  const infoRetirada = document.querySelector('.informa-retirada');
+  if(event.target === this){
+    infoRetirada.classList.add('ativo');
+  }
   
-    button.addEventListener('click', (e) => {
-      if (e.target.value === 'entrega'){
-        parentDados.classList.add('ativo');
-      } else if(parentDados.classList.contains('ativo')){
-        parentDados.classList.remove('ativo');
-      }
-    })
-  })
-};
-ativaDadosEntrega()
-
-
-
+}
